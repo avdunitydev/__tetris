30d6d212cb926d807e3a18cff094f5926702b6d1
Andrii V <avd.g.dev@gmail.com>
2020-11-13 10:33:49 +0200

    Enable all images into canvas.

---
+++ Assets/Scripts/Canvas.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AVDTetris
+{
+    public class Canvas : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Image[] imgs = GetComponentsInChildren<Image>();
+            foreach (var item in imgs)
+            {
+                item.enabled = true;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
